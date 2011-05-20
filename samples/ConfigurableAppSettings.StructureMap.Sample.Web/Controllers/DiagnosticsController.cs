c944db48c65239562c9946cd5705d0210535394f
David Alpert <david@spinthemoose.com>
2011-05-20 16:04:28 -0500

    adding a sample controller action that leverages the IAppSettingsDiagnosticsProvider to show sample appsettings (w/ default values) for all registered settings

---
+++ samples/ConfigurableAppSettings.StructureMap.Sample.Web/Controllers/DiagnosticsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConfigurableAppSettings.StructureMap.Sample.Web.Controllers
+{
+	public class DiagnosticsController : Controller
+	{
+		IAppSettingsDiagnosticsProvider appSettingsDiagnosticsProvider;
+
+		/// <summary>
+		/// Initializes a new instance of the DiagnosticsController class.
+		/// </summary>
+		/// <param name="appSettingsDiagnosticsProvider"></param>
+		public DiagnosticsController( IAppSettingsDiagnosticsProvider appSettingsDiagnosticsProvider )
+		{
+			this.appSettingsDiagnosticsProvider = appSettingsDiagnosticsProvider;
+		}
+
+		// 
+		// GET: /Diagnostics/
+		// NOTE: you can "hide" this route by only mapping it when Debug is enabled, for example.
+		public ActionResult Index()
+		{
+			string sampleConfigEntries = appSettingsDiagnosticsProvider.ExtractSampleSettings();
+
+			return Content( "<pre>"+Server.HtmlEncode( sampleConfigEntries )+"</pre>" );
+		}
+
+	}
+}
